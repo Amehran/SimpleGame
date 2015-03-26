f321cff78c03a6c19bd1e04700329bf4ff304073
Armin Mehran <arminmehran@inside-177-123.wireless.utoronto.ca>
2015-03-26 18:17:40 -0400

    Initial Commit

---
+++ Simple Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple Game
+//
+//  Created by Armin Mehran on 2015-03-26.
+//  Copyright (c) 2015 cd2680. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple Game/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Simple Game
+//
+
+//  Copyright (c) 2015 cd2680. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
